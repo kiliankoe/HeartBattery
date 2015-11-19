42b919c3d76b131f60870b2683560e8a290b55ed
Kilian Koeltzsch <me@kilian.io>
2015-11-19 09:24:34 +0100

    Initial Commit

---
+++ HeartBattery/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HeartBattery
+//
+//  Created by Kilian Költzsch on 19/11/15.
+//  Copyright © 2015 Kilian Költzsch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ HeartBattery/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeartBattery
+//
+//  Created by Kilian Költzsch on 19/11/15.
+//  Copyright © 2015 Kilian Költzsch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
