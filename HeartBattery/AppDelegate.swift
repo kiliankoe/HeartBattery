cf9fd252d8bb9ed3b3a47a80783bb9a5b754c7f5
Kilian Koeltzsch <me@kilian.io>
2015-11-19 19:30:04 +0100

    basic functionality working with emoji hearts ❤️

--- HeartBattery/AppDelegate.swift
+++ HeartBattery/AppDelegate.swift
@@ -7,20 +7,57 @@
 //
 
 import Cocoa
+import SwiftyTimer
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+	
+	@IBOutlet weak var statusMenu: NSMenu!
+	let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+	
+	@IBAction func quitClicked(sender: NSMenuItem) {
+		NSApplication.sharedApplication().terminate(self)
+	}
+	
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
+		statusItem.menu = statusMenu
+		
+		updateStatusBar()
+		
+		NSTimer.every(5.minutes) {
+			self.updateStatusBar()
+		}
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
-
+	
+	func updateStatusBar() {
+		// Get battery information via SystemKit
+		var battery = Battery()
+		if battery.open() != kIOReturnSuccess { exit(0) }
+		let charge = battery.charge()
+		battery.close()
+		
+		// How many hearts do we need?
+		let totalHeartCount = 10
+		let fullHeartCount = Int(round(charge/Double(totalHeartCount)))
+		let emptyHeartCount = totalHeartCount - fullHeartCount
+		
+		let heart_full = NSImage(named: "heart_full")
+		let heart_empty = NSImage(named: "heart_empty")
+	
+//		let resultImage = NSImage(size: CGSizeMake())
+		print("FULL: \(fullHeartCount)0%")
+		var title = ""
+		for _ in 0..<fullHeartCount {
+			title += "❤️"
+		}
+		for _ in 0..<emptyHeartCount {
+			title += "💔"
+		}
+		statusItem.title = title
+	}
 
 }
-
